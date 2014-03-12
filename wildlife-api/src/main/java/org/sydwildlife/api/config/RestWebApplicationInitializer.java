10b8acdf1c57beda291bc2bd5441fe0e77c639bd
Mathieu <mcolombet@vmware.com>
2014-03-13 09:45:56 +1100

    Add Pageable support through Rest

---
+++ wildlife-api/src/main/java/org/sydwildlife/api/config/RestWebApplicationInitializer.java
@@ -0,0 +1,34 @@
+package org.sydwildlife.api.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class RestWebApplicationInitializer implements WebApplicationInitializer {
+
+   @Override
+   public void onStartup(ServletContext servletContext) throws ServletException {
+
+      // setup the ContextLoaderListener listener
+      XmlWebApplicationContext rootContext = new XmlWebApplicationContext();
+      rootContext.setConfigLocations(new String[] {
+            "classpath*:/META-INF/spring-service-context.xml" });
+      servletContext.addListener(new ContextLoaderListener(rootContext));
+
+      XmlWebApplicationContext appContext = new XmlWebApplicationContext();
+      appContext.setConfigLocation("/META-INF/spring/spring-mvc-context.xml");
+
+      // setup the Spring dispatcher servlet listener
+      ServletRegistration.Dynamic registration =
+            servletContext.addServlet("dispatcher", new DispatcherServlet(appContext));
+      registration.setLoadOnStartup(1);
+      registration.addMapping("/");
+
+   }
+
+}
