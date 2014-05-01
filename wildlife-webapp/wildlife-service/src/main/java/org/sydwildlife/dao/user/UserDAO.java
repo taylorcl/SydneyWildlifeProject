093723e97c5a75de3b2443e7886d7f99e21de222
Clement Cureau <clement.cureau@gmail.com>
2014-05-01 17:28:32 +1000

    Removing the old project

--- wildlife-webapp/wildlife-service/src/main/java/org/sydwildlife/dao/user/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package org.sydwildlife.dao.user;
-
- 
-import org.sydwildlife.dao.util.GenericDAO;
-import org.sydwildlife.domain.User;
-
-public interface UserDAO extends GenericDAO<User> {
-	User findByLastName(String lastName);
-}
--- wildlife-webapp/wildlife-web/src/test/lib/jasmine-jquery-settings/jasmine_spec_runner_fixtures_path.js
+++
@@ -1,1 +0,0 @@
-jasmine.getJSONFixtures().fixturesPath = 'src/test/webapp/js/spec/javascripts/fixtures/json';
