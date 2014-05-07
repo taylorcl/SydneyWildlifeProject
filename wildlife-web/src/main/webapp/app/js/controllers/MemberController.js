3ca5ec0bd170dd685caa32ce5681ba79e304d9c2
Clement Cureau <clement.cureau@gmail.com>
2014-05-07 16:29:28 +1000

    Cleaned up a bit

--- wildlife-web/src/main/webapp/app/js/controllers/MemberController.js
+++ wildlife-web/src/main/webapp/app/js/controllers/MemberController.js
@@ -1,25 +1,32 @@
 'use strict';
 
-/*
+/**
  * Members functionalities for the controller layer
  */
 sydneyWildlifeApp.controller('MemberController',
     function MemberController($scope, $window, $timeout, MemberService, NavService, AlertService, $routeParams, NAV_PATHS, USER_ROLES, ALERT_CODES) {
-	    $scope.originalMember = null;
-	    $scope.memberDetailLoading = false;
+       $scope.memberList = {};
+	    $scope.originalMember = {};
+	    $scope.member = {};
+	    $scope.waitingForMemberDetails = false;
 	    $scope.editMode = false;
+	    $scope.errors = [];
 	    	    
 	    /**
-	     * Save member
+	     * Saves the member by calling the back-end.
+	     * 
+	     * @param member The member object
+	     * @param form The form object attached to the "form" HTML tag
 	     */
 	    $scope.saveMember = function(member, form) {
             if(form.$valid) {
             	var aPromise = MemberService.save(member);
             	aPromise.then(function(object) {
             	   member.id = object.id;
-            	   AlertService.show(ALERT_CODES.success, "Successfully registered/updated member " + member.firstName + " " + member.lastName + ".");
-            	   NavService.goTo(NAV_PATHS.memberList);
+            	   AlertService.show(ALERT_CODES.success, "Successfully saved member #" + member.id + "(" + member.firstName + " " + member.lastName + ").");
+            	   $scope.goBack();
             	}, function errorCallback(error) {
+            	   $scope.errors.push("We cannot save the member #" + member.id + " (" + member.firstName + " " + member.lastName + ") at the moment, please try again later.");
             	   AlertService.show(ALERT_CODES.error, "Error saving member with Id " + member.id + ".");
             	});
             } else {
@@ -28,36 +35,40 @@ sydneyWildlifeApp.controller('MemberController',
         };
         
         /**
-         * List members
+         * List members.
          */
         $scope.listMembers = function() {
-        	MemberService.list().then(function(object) {
-        		$scope.memberList = object;
-        	}, function(error) {
-        	   AlertService.show(ALERT_CODES.error, "Error retrieving members.");
-        	});
+           	MemberService.list().then(function(object) {
+           		$scope.memberList = object;
+           	}, function(error) {
+           	   $scope.errors.push("We cannot retrieve the list of members at the moment, please try again later.");
+           	   AlertService.show(ALERT_CODES.error, "Error retrieving members.");
+           	});
         };
         
         /**
-         * Retrieve one member
+         * Retrieve one member. Takes the id from the route parameters.
          */
         $scope.memberDetail = function() {
         	if ($routeParams != undefined && $routeParams.memberId != undefined) {
-        	   $scope.memberDetailLoading = true;
+        	   $scope.waitingForMemberDetails = true;
         		MemberService.memberDetail($routeParams.memberId).get().then(function(object) {
-        		   $scope.memberDetailLoading = false;
+        		   $scope.waitingForMemberDetails = false;
         			$scope.member = object.originalData;
         			//do a clone of the original object
         			$scope.originalMember = JSON.parse(JSON.stringify(object.originalData));
 	        	}, function(error) {
-	        	   $scope.memberDetailLoading = false;
+	        	   $scope.waitingForMemberDetails = false;
+	        	   $scope.errors.push("We cannot retrieve the member #" + $routeParams.memberId + " at the moment, please try again later.");
 	        		AlertService.show(ALERT_CODES.error, "Error retrieving member with Id " + $routeParams.memberId + ".");
 	        	});
         	}
         };
         
         /**
-         * Delete one member
+         * Delete one member.
+         * 
+         * @param member The member object
          */
         $scope.deleteMember = function(member) {
         	if (member != undefined && member.id != undefined){
@@ -69,30 +80,56 @@ sydneyWildlifeApp.controller('MemberController',
               		MemberService.deleteMember(member.id).then(function(object) {
               			$scope.member = {};
               			AlertService.show(ALERT_CODES.info, "Successfully deleted member with Id "+ member.id + "." );
-              			NavService.goTo(NAV_PATHS.memberList);
+              			$scope.goBack();
       	        	}, function(error) {
+      	        	   $scope.errors.push("We cannot delete the member #" + member.id + " (" + member.firstName + " " + member.lastName + ") at the moment, please try again later.");
       	        		AlertService.show(ALERT_CODES.error, "Error deleting member with Id " + member.id + ".");
-      	        		NavService.goTo(NAV_PATHS.memberList);
       	        	});
            	   }
         	   });
         	}
         };
         
-        $scope.cancel = function() {
+        /**
+         * Goes back to the Members List page id admin role or to the home page otherwise
+         */
+        $scope.goBack = function() {
            if($scope.isAuthorized(USER_ROLES.admin)) {
               NavService.goTo(NAV_PATHS.memberList);
            } else {
               NavService.goTo(NAV_PATHS.home);
            }
         };
+        
+        /**
+         * Goes back.
+         */
+        $scope.cancel = function() {
+           $scope.goBack();
+        };
 
+        /**
+         * Returns true if the member object is a new one, i.e. it hasn't been saved to the DB yet.
+         */
         $scope.isNew = function() {
-           //shouldn't retrun true when an error occurred for retrieving a member
-           return !$scope.memberDetailLoading && ($scope.member == undefined || $scope.member.id == undefined);
+           return !$scope.waitingForMemberDetails && ($scope.member == undefined || $scope.member.id == undefined);
         };
         
+        /**
+         * Returns true if this view has errors.
+         */
+        $scope.hasErrors = function() {
+           return $scope.errors.length > 0;
+        };
+        
+        /**
+         * Returns a string value representing a form mode: "create", "read" or "update". If any error occurred, the result will be "error".
+         */
         $scope.getMode = function() {
+           if($scope.hasErrors()) {
+              return "error";
+           }
+           
            if($scope.isNew()) {
               return "create";
            } else {
@@ -104,10 +141,16 @@ sydneyWildlifeApp.controller('MemberController',
            }
         };
         
+        /**
+         * Starts the "update" mode.
+         */
         $scope.startEdit = function () {
            $scope.editMode = true;
         };
         
+        /**
+         * Stops the "update" mode without saving changes.
+         */
         $scope.cancelEdit = function () {
            $scope.editMode = false;
         };
